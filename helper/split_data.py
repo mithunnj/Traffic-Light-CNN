ae872139764bf53e1b4a04f70a1438b414697e74
Mithun Jothiravi <mithun.jothiravi@mail.utoronto.ca>
2020-08-04 19:12:38 -0400

    Modified dataset split by splitting original Bosch Traffic Light dataset instead of adding additional images that were taken from Iphone.

--- helper/split_data.py
+++ helper/split_data.py
@@ -21,26 +21,26 @@ def split_dataset(CROP_IMGS):
     directories = os.listdir(CROP_IMGS)
 
     for directory in directories:
-        if ".DS_Store" in directory: # This is a corner case folder that is generated
+        if ".DS_Store" in directory or "IMG_" in directory: # This is a corner case folder that is generated
             continue
 
         sub_files = os.listdir('{}/{}'.format(CROP_IMGS, directory))
         sub_files.remove("file.name")
 
-        if "IMG_" in directory: # All holdout test set files have the following directory names: IMG_<number>
-            for sub_file in sub_files:
+        for sub_file in sub_files:
+            if file_count < 17521: # ~60% of the total object detected dataset (29201 objects) for the Train Dataset
+                file_count += 1
+                data_split['train'].append("/{}/{}".format(directory, sub_file))
+            elif 17521 <= file_count <= 23360: # ~20% of the total object detected dataset for the Validation Dataset
+                file_count += 1
+                data_split['val'].append("/{}/{}".format(directory, sub_file))
+            elif 23360 < file_count: # ~20% of the total object detected dataset for the holdout test dataset
+                file_count += 1
                 data_split['test'].append("/{}/{}".format(directory, sub_file))
-        else:
-            for sub_file in sub_files:
-                if file_count < 17521: # ~60% of the total object detected dataset (29201 objects) for the Train Dataset
-                    file_count += 1
-                    data_split['train'].append("/{}/{}".format(directory, sub_file))
-                else: # ~40% of the totoal object detected dataset for the Validation Dataset
-                    file_count += 1
-                    data_split['val'].append("/{}/{}".format(directory, sub_file))
 
     #print("Length of train set: {}".format(len(data_split['train'])))
     #print("Length of validation set: {}".format(len(data_split['val'])))
     #print("Length of test set: {}".format(len(data_split['test'])))
+
 
     return data_split
